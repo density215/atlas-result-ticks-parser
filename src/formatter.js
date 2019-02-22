8dd9ad31738d00b3ccb48b707e53e12a806fd512
Jasper den Hertog <jdenhertog@ripe.net>
2019-02-22 11:46:30 +0100

    add Duration stats / output arrays

---
+++ src/formatter.js
@@ -0,0 +1,115 @@
+import assert from "assert";
+import math from "mathjs";
+
+// We don't use TypedArrays since they are not compatible with mathjs.
+// But this seems reasonably fast.
+
+// Given an array `statesArr` of indicator (state) variables, and an array `rttArr` of observations,
+// returns an array for each cluster of observtions.
+//
+// statesArr = [1,1,2,2,1,1]
+// rttArr = [1,2,3,4,5,6]
+
+// clusters : [ { state: <STRING>, rtt: [<NUM>,<NUM>,...]}]
+const getClusters = (statesArr, rttArr) => {
+  assert(statesArr.length === rttArr.length);
+  return statesArr.reduce((clustersArr, state, i) => {
+    let existState = clustersArr.find(c => c.state === state);
+    let newState = {
+      state: state,
+      rtt: [...((existState && existState.rtt) || []), rttArr[i]]
+    };
+    if (existState) {
+      clustersArr[clustersArr.indexOf(existState)] = newState;
+    } else {
+      clustersArr.push(newState);
+    }
+    return clustersArr;
+  }, []);
+};
+
+function getClusterStats(cluster) {
+  console.log("00000");
+  console.log(cluster);
+  console.log("----");
+  let min = math.min(cluster.rtt);
+  let max = math.max(cluster.rtt);
+  let med = math.median(cluster.rtt);
+  // TODO: IQR
+
+  return {
+    min: min,
+    max: max,
+    median: med
+  };
+}
+
+// Ranges are Python-style (inclusive on the left and exclusive on the right)
+// i.e. [a,b).
+function getSegments(stateseq, timestamps) {
+  assert(stateseq.length == timestamps.length);
+
+  let lastIdx = 0;
+  let lastState = stateseq[0];
+  let segments = [];
+
+  for (let i = 0; i < stateseq.length; i++) {
+    if (stateseq[i] != lastState || i == stateseq.length - 1) {
+      segments.push({
+        state: lastState,
+        start: lastIdx,
+        stop: i,
+        startTime: timestamps[lastIdx],
+        stopTime: timestamps[i]
+      });
+
+      lastIdx = i;
+      lastState = stateseq[i];
+    }
+  }
+
+  return segments;
+}
+
+export function createSummary({
+  stateseq,
+  timestamps,
+  rtt,
+  maxTimeStamp,
+  minTimeStamp
+}) {
+  let segments = getSegments(stateseq, timestamps);
+  let clusters = getClusters(stateseq, rtt);
+  let statesCount = Object.keys(clusters).length;
+
+  const clustersStats = clusters.map(c => ({
+    state: c.state,
+    rtt_summary: getClusterStats(c)
+  }));
+
+  const clustersStatsWithTotalDuration = clustersStats.map(c => {
+    const total_duration = segments
+      .filter(seg => seg.state === c.state)
+      .reduce(
+        (totalDuration, thisSeg) =>
+          totalDuration + (thisSeg.stopTime - thisSeg.startTime),
+        0
+      );
+    return {
+      ...c,
+      summed_duration: total_duration,
+      summed_duration_as_percentage_of_total:
+        (total_duration / (maxTimeStamp - minTimeStamp)) * 100
+    };
+  });
+
+  return {
+    statesCount: statesCount,
+    states: clustersStatsWithTotalDuration.sort(
+      (a, b) =>
+        a.summed_duration_as_percentage_of_total <
+        b.summed_duration_as_percentage_of_total
+    ),
+    segments: segments
+  };
+}
