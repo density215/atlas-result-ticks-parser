f76e431c9da150c32bff0c5d78d3d4dbf7f5cf19
Jasper den Hertog <jdenhertog@ripe.net>
2019-03-06 14:36:09 +0100

    time validators

--- src/validators.js
+++ src/validators.js
@@ -1,5 +1,8 @@
 import { DateTime } from "luxon";
 
+const dTorDate = dt =>
+  (dt instanceof Date && DateTime.fromJSDate(dt)) || (dt.isValid && dt) || null;
+
 export const validateCommaSeparatedListWithNumbers = list =>
   list.split(",").every(n => validateNumber(n));
 
@@ -24,13 +27,19 @@ export const validateDateTimeAsISOorTimeStamp = datetime =>
   !DateTime.fromISO(datetime).invalid ||
   !DateTime.fromSeconds(parseInt(datetime)).invalid;
 
+export const validateNotInTheFuture = datetime => {
+  const now = DateTime.utc();
+  const dt = dTorDate(datetime);
+  return dt ? now > dt : false;
+};
+
 export const validateMsmOrRejectMessages = msmMetaData => {
   let validateRejection = [];
   // we need at least 100 ticks
   const minDuration = 100 * msmMetaData.interval;
   if (msmMetaData.is_oneoff) {
     validateRejection.push(
-      "Not a recurring measuremnent. Trends are only available for recurring measurements."
+      "Not a recurring measurement. Trends are only available for recurring measurements."
     );
   }
   if (msmMetaData.type !== "ping") {
@@ -48,4 +57,32 @@ export const validateMsmOrRejectMessages = msmMetaData => {
     );
   }
   return (validateRejection.length === 0 && true) || validateRejection;
+};
+
+export const validateHasEnoughTicks = minTicks => ({
+  interval,
+  startTime,
+  stopTime
+}) => {
+  startTime = dTorDate(startTime);
+  stopTime = dTorDate(stopTime);
+  const duration = stopTime.diff(startTime).as("seconds") ;
+  const ticksNo = Math.floor(duration / interval);
+  const enoughTicks = ticksNo >= minTicks;
+  
+  return enoughTicks
+    ? true
+    : `Not enough ticks in specified time range. Trends need at least 100 non-empty results ('ticks'). Found ${ticksNo} ticks. Enlarging time range with ${(minTicks * interval - duration)/ 3600} hours will probably yield results.`;
+};
+
+// Takes a mix of JS Date objects or luxon DateTime objects and
+// compares them to be within the maxDuration.
+// maxDuration is a luxon Duration object.
+export const validateMaxDuration = maxDuration => (startDt, stopDt) => {
+  startDt = dTorDate(startDt);
+  stopDt = dTorDate(stopDt);
+  if (!startDt || !stopDt) {
+    return false;
+  }
+  return stopDt.diff(startDt) < maxDuration;
 };
